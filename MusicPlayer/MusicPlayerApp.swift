763d15c4f869aa0d73102ef82d150361c157fa5b
Kévin Puyjalinet <puyjalinet.kevin@gmail.com>
2025-04-04 14:44:18 +0200

    Initial Commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Kévin PUYJALINET on 04/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
